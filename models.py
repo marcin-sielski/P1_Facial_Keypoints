a76bf5e6ac0c9a9631deb687b668d1826c27dbb4
Marcin Sielski <marcin.sielski@gmail.com>
2021-06-12 21:45:56 +0200

    Initial commit

--- models.py
+++ models.py
@@ -20,7 +20,18 @@ class Net(nn.Module):
         
         # As an example, you've been given a convolutional layer, which you may (but don't have to) change:
         # 1 input image channel (grayscale), 32 output channels/feature maps, 5x5 square convolution kernel
-        self.conv1 = nn.Conv2d(1, 32, 5)
+        self.conv1 = nn.Conv2d(1, 128, 5)
+        #self.conv2 = nn.Conv2d(128, 128, 5)
+        self.conv3 = nn.Conv2d(128, 64, 5)
+        
+        self.pool = nn.MaxPool2d(2, 2)
+        
+        self.fc1_drop = nn.Dropout(p=0.3)
+        
+        self.fc1 = nn.Linear(179776, 1000)
+        self.fc2 = nn.Linear(1000, 500)
+        
+        self.fc3 = nn.Linear(500, 136)
         
         ## Note that among the layers to add, consider including:
         # maxpooling layers, multiple conv layers, fully-connected layers, and other layers (such as dropout or batch normalization) to avoid overfitting
@@ -31,7 +42,23 @@ class Net(nn.Module):
         ## TODO: Define the feedforward behavior of this model
         ## x is the input image and, as an example, here you may choose to include a pool/conv step:
         ## x = self.pool(F.relu(self.conv1(x)))
+        x = self.pool(F.relu(self.conv1(x)))
+        x = self.fc1_drop(x)
+        #x = F.relu(self.conv2(x))
+        #x = self.fc1_drop(x)
+        x = self.pool(F.relu(self.conv3(x)))
+        x = self.fc1_drop(x)
+
+        # prep for linear layer
+        # this line of code is the equivalent of Flatten in Keras
+        x = x.view(x.size(0), -1)
         
+        # two linear layers with dropout in between
+        x = F.relu(self.fc1(x))
+        x = self.fc1_drop(x)
+        x = F.relu(self.fc2(x))
+        x = self.fc1_drop(x)
+        x = self.fc3(x)
         
         # a modified x, having gone through all the layers of your model, should be returned
         return x
