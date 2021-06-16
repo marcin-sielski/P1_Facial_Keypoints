5b0913d6c2d6dc3247912a44873327ef8ab616c6
Marcin Sielski <marcin.sielski@gmail.com>
2021-06-16 19:33:24 +0200

    Initial commit

--- models.py
+++ models.py
@@ -20,16 +20,20 @@ class Net(nn.Module):
         
         # As an example, you've been given a convolutional layer, which you may (but don't have to) change:
         # 1 input image channel (grayscale), 32 output channels/feature maps, 5x5 square convolution kernel
-        self.conv1 = nn.Conv2d(1, 128, 5)
-        #self.conv2 = nn.Conv2d(128, 128, 5)
-        self.conv3 = nn.Conv2d(128, 64, 5)
+        self.conv1 = nn.Conv2d(1, 32, 3)
+        #self.tanh = nnTanh()
+        self.conv2 = nn.Conv2d(32, 64, 3)
+        self.conv3 = nn.Conv2d(64, 128, 3)
+        self.conv4 = nn.Conv2d(128, 256, 3)
+        self.conv5 = nn.Conv2d(256, 512, 3)
+        self.conv6 = nn.Conv2d(512, 1024, 3)
         
         self.pool = nn.MaxPool2d(2, 2)
         
-        self.fc1_drop = nn.Dropout(p=0.3)
+        self.drop = nn.Dropout(p=0.5)
         
-        self.fc1 = nn.Linear(179776, 1000)
-        self.fc2 = nn.Linear(1000, 500)
+        self.fc1 = nn.Linear(1024, 500)
+        #self.fc2 = nn.Linear(2500, 500)
         
         self.fc3 = nn.Linear(500, 136)
         
@@ -43,21 +47,32 @@ class Net(nn.Module):
         ## x is the input image and, as an example, here you may choose to include a pool/conv step:
         ## x = self.pool(F.relu(self.conv1(x)))
         x = self.pool(F.relu(self.conv1(x)))
-        x = self.fc1_drop(x)
+        x = self.drop(x)
+        x = self.pool(F.relu(self.conv2(x)))
+        x = self.drop(x)
+        x = self.pool(F.relu(self.conv3(x)))
+        x = self.drop(x)
+        x = self.pool(F.relu(self.conv4(x)))
+        x = self.drop(x)
+        #x = self.fc1_drop(x)
         #x = F.relu(self.conv2(x))
         #x = self.fc1_drop(x)
-        x = self.pool(F.relu(self.conv3(x)))
-        x = self.fc1_drop(x)
+        x = self.pool(F.relu(self.conv5(x)))
+        x = self.drop(x)
+        x = self.pool(F.relu(self.conv6(x)))
+        x = self.drop(x)
 
         # prep for linear layer
         # this line of code is the equivalent of Flatten in Keras
         x = x.view(x.size(0), -1)
         
         # two linear layers with dropout in between
+        #x = self.drop(x)
         x = F.relu(self.fc1(x))
-        x = self.fc1_drop(x)
-        x = F.relu(self.fc2(x))
-        x = self.fc1_drop(x)
+        #x = self.drop(x)
+        #x = F.relu(self.fc2(x))
+        #x = self.drop(x)
+        #x = F.relu(self.fc3(x))
         x = self.fc3(x)
         
         # a modified x, having gone through all the layers of your model, should be returned
